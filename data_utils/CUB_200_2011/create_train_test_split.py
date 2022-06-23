1319a78ee52bf10cb24289067de8d18b5b7acf39
PSRahul <rahulpsr95@gmail.com>
2022-06-23 18:28:08 +0200

    final commit 2306

--- data_utils/CUB_200_2011/create_train_test_split.py
+++ data_utils/CUB_200_2011/create_train_test_split.py
@@ -3,6 +3,11 @@ import os
 
 def main():
     image_root_path = "/home/psrahul/MasterThesis/datasets/CUB_200_2011/CUB_200_2011/images/"
+    class_names = sorted(os.listdir(image_root_path))
+
+    for i in range(len(class_names)):
+        class_name = class_names[i]
+        print(class_name)
 
 
 if __name__ == '__main__':
