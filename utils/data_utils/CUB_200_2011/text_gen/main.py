c48cb42e2a1333fac6267d00793bd608d4158c6c
PSRahul <rahulpsr95@gmail.com>
2022-06-28 11:04:58 +0200

    extract text attributes

--- utils/data_utils/CUB_200_2011/text_gen/main.py
+++ utils/data_utils/CUB_200_2011/text_gen/main.py
@@ -1,5 +1,5 @@
-from email.mime import image
 import pandas as pd
+import numpy as np
 
 
 class DescGenerator():
@@ -11,8 +11,8 @@ class DescGenerator():
 
         class_attribute_labels = "/home/psrahul/MasterThesis/datasets/CUB_200_2011/CUB_200_2011/attributes/class_attribute_labels_continuous.txt"
 
-        # self.attributes = pd.read_csv(attribute_definitions,
-        #                              sep=" ", header=None)
+        self.attributes = pd.read_csv(attribute_definitions,
+                                      sep=" ", header=None)
 
         # self.certainities = pd.read_csv(
         #    attribute_certainities, sep=" ", header=None)
@@ -24,15 +24,61 @@ class DescGenerator():
             class_attribute_labels, sep=" ", header=None)
 
     def process(self):
-        # print(self.attributes.head())
-        # print(self.image_attributes.head())
-        # print(self.certainities.head())
-        print(self.class_attribute_labels.head())
+        self.attribute_series_t5_list = []
+        self.attribute_series_t3_list = []
+        self.attribute_series_t1_list = []
+        for idx in range(200):
+            class_series = self.class_attribute_labels.iloc[idx].sort_values(
+                ascending=False).keys()
+            attribute_series_t5 = class_series[0:5]
+            attribute_series_t3 = class_series[0:3]
+            attribute_series_t1 = class_series[0]
+            self.attribute_series_t5_list.append(
+                np.array(attribute_series_t5))
+            self.attribute_series_t3_list.append(
+                np.array(attribute_series_t3))
+            self.attribute_series_t1_list.append(
+                np.array(attribute_series_t1))
+
+    def __repr__(self):
+        print(self.attribute_series_t5_list)
+        print("\n")
+        print(self.attribute_series_t3_list)
+        print("\n")
+        print(self.attribute_series_t1_list)
+
+        return " "
+
+    def get_index_class_list(self, attrib_list):
+        self.attribute_series_text = []
+
+        for idx in range(200):
+            index_text = []
+            # print(attrib_list)
+            for attribute in list(attrib_list[idx].ravel()):
+                # print(attribute)
+                # print(self.attributes.iloc[int(attribute)])
+                index_text.append(self.attributes.iloc[int(attribute)][1])
+            self.attribute_series_text.append(index_text)
+
+        return self.attribute_series_text
+
+    def get_class_text(self):
+        self.attribute_series_t5_text = self.get_index_class_list(
+            self.attribute_series_t5_list)
+        self.attribute_series_t3_text = self.get_index_class_list(
+            self.attribute_series_t3_list)
+        self.attribute_series_t1_text = self.get_index_class_list(
+            self.attribute_series_t1_list)
 
 
 def main():
     descgenerator = DescGenerator()
     descgenerator.process()
+    descgenerator.get_class_text()
+    print(descgenerator.attribute_series_t5_text)
+    print(descgenerator.attribute_series_t3_text)
+    print(descgenerator.attribute_series_t1_text)
 
 
 if __name__ == "__main__":
