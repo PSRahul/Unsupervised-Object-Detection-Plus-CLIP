57d927e892e20147b2732a0641c484d88e57a6ba
PSRahul <rahulpsr95@gmail.com>
2022-06-29 11:41:04 +0200

    change text and image to support and query

--- similarity_metrics/get_similarity_metric.py
+++ similarity_metrics/get_similarity_metric.py
@@ -8,24 +8,25 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, multilabel
 
 
 class GetSimilarity():
-    def __init__(self, image_feature_path, text_feature_path):
-        self.text_features = torch.load(text_feature_path)
-        self.image_features = torch.load(image_feature_path)
+    def __init__(self, query_feature_path, support_feature_path):
+        self.support_features = torch.load(support_feature_path)
+        self.query_features = torch.load(query_feature_path)
 
     def get_predictions(self):
-        self.text_features /= self.text_features.norm(dim=-1, keepdim=True)
-        self.image_features /= self.image_features.norm(dim=-1, keepdim=True)
+        self.support_features /= self.support_features.norm(
+            dim=-1, keepdim=True)
+        self.query_features /= self.query_features.norm(dim=-1, keepdim=True)
 
-        text_probs = (100.0 * self.image_features @
-                      self.text_features.T).softmax(dim=-1)
-        top_probs, top_labels = text_probs.cpu().topk(1, dim=-1)
+        support_probs = (100.0 * self.query_features @
+                         self.support_features.T).softmax(dim=-1)
+        top_probs, top_labels = support_probs.cpu().topk(1, dim=-1)
         self.predictions = top_labels.numpy().ravel()
 
     def get_targets(self):
 
-        image_label_txt = "/home/psrahul/MasterThesis/repo/Unsupervised-Object-Detection-Plus-CLIP/utils/data_utils/CUB_200_2011/data_splits/test/image_class_labels.txt"
+        query_label_txt = "/home/psrahul/MasterThesis/repo/Unsupervised-Object-Detection-Plus-CLIP/utils/data_utils/CUB_200_2011/data_splits/test/image_class_labels.txt"
 
-        file = open(image_label_txt, 'r')
+        file = open(query_label_txt, 'r')
         file_Lines = file.readlines()
 
         target_labels = []
@@ -49,20 +50,20 @@ class GetSimilarity():
             f1_score(self.targets, self.predictions, average="macro") * 100))
 
 
-text_feature_path = "//home/psrahul/MasterThesis/repo/Unsupervised-Object-Detection-Plus-CLIP/utils/data_utils/CUB_200_2011/text_features_v2/" + \
+support_feature_path = "//home/psrahul/MasterThesis/repo/Unsupervised-Object-Detection-Plus-CLIP/utils/data_utils/CUB_200_2011/text_features_v2/" + \
     "text_feature_just_class_names.pt"
 #   "text_feature_just_class_names.pt"
 # "text_feature_t5_text.pt"
 # "text_feature_t3_text.pt"
 # "text_feature_t1_text.pt"
 
-image_feature_path = "/home/psrahul/MasterThesis/repo/Unsupervised-Object-Detection-Plus-CLIP/utils/data_utils/CUB_200_2011/image_features/" + \
+query_feature_path = "/home/psrahul/MasterThesis/repo/Unsupervised-Object-Detection-Plus-CLIP/utils/data_utils/CUB_200_2011/image_features/" + \
     "image_feature_cropped.pt"
-# "image_feature_full.pt"
-# "image_feature_cropped.pt"
+# "query_feature_full.pt"
+# "query_feature_cropped.pt"
 
-get_similarity = GetSimilarity(text_feature_path=text_feature_path,
-                               image_feature_path=image_feature_path)
+get_similarity = GetSimilarity(support_feature_path=support_feature_path,
+                               query_feature_path=query_feature_path)
 
 get_similarity.get_accuracy_score()
 get_similarity.get_f1_score()
