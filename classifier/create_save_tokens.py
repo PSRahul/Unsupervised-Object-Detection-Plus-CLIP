c81a100c32dad058cf1be348162be9210e72f0ea
PSRahul <rahulpsr95@gmail.com>
2022-06-29 14:18:09 +0200

    extracted image features for all training images

--- classifier/create_save_tokens.py
+++ classifier/create_save_tokens.py
@@ -1,5 +1,6 @@
 # %%
 
+from subprocess import call
 from clip import tokenize
 import numpy as np
 import torch
@@ -8,78 +9,8 @@ from PIL import Image
 from clip import available_models
 import sys
 import clip
-# %%
-
-
-def get_unique_encodings(class_name_list):
-    for i in range(len(class_name_list)):
-        print(class_name_list[i], class_token_list[i],
-              np.sum(np.array(class_token_list[i])))
-
-    print(np.array(class_token_list).shape)
-
-    print(len(class_token_list))
-    print(np.unique(np.sum(np.array(class_token_list), axis=1)).shape)
-
-
-# %%
-
-
-def create_token_images_from_classes_files():
-    class_names_txt = "/home/psrahul/MasterThesis/datasets/CUB_200_2011/CUB_200_2011/classes.txt"
-    class_name_list = []
-    class_token_list = []
-    file = open(class_names_txt, 'r')
-    file_Lines = file.readlines()
-    for file_line in file_Lines:
-        file_line = file_line.strip()
-        class_name = file_line.split(".")[1]
-        class_name_list.append(class_name)
-        class_token_list.append(
-            tokenize("This is " + str(class_name)).numpy().ravel())
-
-    class_token_list = np.array(class_token_list)
-    text_tokens = torch.from_numpy(class_token_list)
-
-    torch.save(text_tokens, "/home/psrahul/MasterThesis/repo/Unsupervised-Object-Detection-Plus-CLIP/CLIP/encodings/text_tokens.pt")
-    # %%
-    image_root_path = "/home/psrahul/MasterThesis/datasets/CUB_200_2011/CUB_200_2011/test/"
-    train_image_path = "/home/psrahul/MasterThesis/repo/Unsupervised-Object-Detection-Plus-CLIP/data_utils/CUB_200_2011/test/images.txt"
-    class_names = sorted(os.listdir(image_root_path))
-    file = open(train_image_path, 'r')
-    file_Lines = file.readlines()
-    model, preprocess = clip.load("ViT-B/16")
-    image_list = []
-    for file_line in file_Lines:
-        file_line = file_line.strip()
-        idx, file_name = file_line.split(" ")
-        image = Image.open(os.path.join(
-            image_root_path, file_name)).convert("RGB")
-        image_list.append(preprocess(image))
-
-    image_input = torch.tensor(np.stack(image_list)).cuda()
-
-    torch.save(image_input, "/home/psrahul/MasterThesis/repo/Unsupervised-Object-Detection-Plus-CLIP/CLIP/encodings/processed_images.pt")
-
-
-# %%
-# create_token_images()
-
-
-def get_text_features(root):
-    text_tokens = torch.load(
-        "/home/psrahul/MasterThesis/repo/Unsupervised-Object-Detection-Plus-CLIP/CLIP/encodings/text_tokens_cropped.pt")
-
-    model, preprocess = clip.load("ViT-B/16")
-    text_features = torch.zeros((text_tokens.shape[0], 512))
-
-    with torch.no_grad():
-        for i in range(text_tokens.shape[0]):
-            text_token = text_tokens[i].cuda().reshape((1, 77))
-            text_feat = model.encode_text(text_token).float()
-            text_features[i] = text_feat
-
-    torch.save(text_features, "/home/psrahul/MasterThesis/repo/Unsupervised-Object-Detection-Plus-CLIP/CLIP/encodings/text_feature_cropped.pt")
+import time
+from tqdm import tqdm
 
 
 def get_image_features():
@@ -89,70 +20,13 @@ def get_image_features():
     model, preprocess = clip.load("ViT-B/16")
     image_features = torch.zeros((image_input.shape[0], 512))
     with torch.no_grad():
-        for i in range(image_input.shape[0]):
+        for i in tqdm(range(image_input.shape[0])):
             image = image_input[i].unsqueeze(0).cuda()
             image_feat = model.encode_image(image).float()
             image_features[i] = image_feat
 
     torch.save(image_features, "/home/psrahul/MasterThesis/repo/Unsupervised-Object-Detection-Plus-CLIP/CLIP/encodings/image_features_cropped.pt")
 
-
-# create_token_images()
-# get_text_features()
-# get_image_features()
-
-
-def create_token_images_with_bounding_boxes():
-    class_names_txt = "/home/psrahul/MasterThesis/datasets/CUB_200_2011/CUB_200_2011/classes.txt"
-    class_name_list = []
-    class_token_list = []
-    file = open(class_names_txt, 'r')
-    file_Lines = file.readlines()
-    for file_line in file_Lines:
-        file_line = file_line.strip()
-        class_name = file_line.split(".")[1]
-        class_name_list.append(class_name)
-        class_token_list.append(
-            tokenize("This is a photo of a " + str(class_name)).numpy().ravel())
-
-    class_token_list = np.array(class_token_list)
-    text_tokens = torch.from_numpy(class_token_list)
-
-    torch.save(text_tokens, "/home/psrahul/MasterThesis/repo/Unsupervised-Object-Detection-Plus-CLIP/CLIP/encodings/text_tokens_cropped.pt")
-    # %%
-    image_root_path = "/home/psrahul/MasterThesis/datasets/CUB_200_2011/CUB_200_2011/test/CUB_200_2011/images/"
-    train_image_path = "/home/psrahul/MasterThesis/repo/Unsupervised-Object-Detection-Plus-CLIP/data_utils/CUB_200_2011/test/images.txt"
-    class_names = sorted(os.listdir(image_root_path))
-    file = open(train_image_path, 'r')
-    file_Lines = file.readlines()
-    model, preprocess = clip.load("ViT-B/16")
-    image_list = []
-    for file_line in file_Lines:
-        file_line = file_line.strip()
-        idx, file_name = file_line.split(" ")
-        print(os.path.join(
-            image_root_path, file_name))
-        image = Image.open(os.path.join(
-            image_root_path, file_name)).convert("RGB")
-        # image.show()
-        # print(file_name.split("/")[1].split(".")[0])
-        pred = np.load(os.path.join(
-            "/home/psrahul/MasterThesis/repo/Unsupervised-Object-Detection-Plus-CLIP/datasets/CUB_200_2011/bounding_boxes/TokenCut-vit_small16_k/", str(file_name.split("/")[1].split(".")[0] + ".npy")))
-        # print(pred)
-        image_cropped = image.crop(pred)
-        image_list.append(preprocess(image_cropped))
-
-    image_input = torch.tensor(np.stack(image_list)).cuda()
-
-    sys.exit(0)
-    torch.save(image_input, "/home/psrahul/MasterThesis/repo/Unsupervised-Object-Detection-Plus-CLIP/CLIP/encodings/processed_images_cropped.pt")
-
-
-# create_token_images_with_bounding_boxes()
-
-# create_token_images_with_bounding_boxes()
-# get_text_features()
-# get_image_features()
 
 def get_text_token_from_desc_list(load_name, save_name, load_file_root="/home/psrahul/MasterThesis/repo/Unsupervised-Object-Detection-Plus-CLIP/utils/data_utils/CUB_200_2011/text_gen/text_tokens_v2/", save_file_root="/home/psrahul/MasterThesis/repo/Unsupervised-Object-Detection-Plus-CLIP/utils/data_utils/CUB_200_2011/text_gen/text_tokens_v2/"):
 
@@ -169,11 +43,56 @@ def get_text_token_from_desc_list(load_name, save_name, load_file_root="/home/ps
     torch.save(text_tokens, os.path.join(save_file_root, save_name))
 
 
-get_text_token_from_desc_list(
-    load_name="just_class_names.pt", save_name="text_token_just_class_names.pt")
-get_text_token_from_desc_list(
-    load_name="t5_text.pt", save_name="text_token_t5_text.pt")
-get_text_token_from_desc_list(
-    load_name="t3_text.pt", save_name="text_token_t3_text.pt")
-get_text_token_from_desc_list(
-    load_name="t1_text.pt", save_name="text_token_t1_text.pt")
+def call_get_text_token_from_desc_list():
+    get_text_token_from_desc_list(
+        load_name="just_class_names.pt", save_name="text_token_just_class_names.pt")
+    get_text_token_from_desc_list(
+        load_name="t5_text.pt", save_name="text_token_t5_text.pt")
+    get_text_token_from_desc_list(
+        load_name="t3_text.pt", save_name="text_token_t3_text.pt")
+    get_text_token_from_desc_list(
+        load_name="t1_text.pt", save_name="text_token_t1_text.pt")
+
+
+def create_image_features_from_classes_folders(images_path, images_list, save_folder, save_name, crop_bounding_box=False):
+
+    file = open(images_list, 'r')
+    file_Lines = file.readlines()
+    model, preprocess = clip.load("ViT-B/16")
+    image_list = []
+    image_features = torch.zeros((len(file_Lines), 512))
+
+    for i, file_line in enumerate(tqdm(file_Lines)):
+        time.sleep(0.1)
+        file_line = file_line.strip()
+        idx, file_name = file_line.split(" ")
+        image = Image.open(os.path.join(
+            images_path, file_name)).convert("RGB")
+
+        if(crop_bounding_box):
+            pred = np.load(os.path.join(
+                "/home/psrahul/MasterThesis/repo/Unsupervised-Object-Detection-Plus-CLIP/bounding_box_regression/outputs/TokenCut/CUB200/bounding_boxes/TokenCut-vit_small16_k", str(file_name.split("/")[1].split(".")[0] + ".npy")))
+            # print(pred)
+            image = image.crop(pred)
+        with torch.no_grad():
+            image = torch.tensor(preprocess(
+                image)).detach().unsqueeze(0).cuda()
+            image_feat = model.encode_image(image).float()
+        image_features[i] = image_feat
+
+    torch.save(image_features, os.path.join(save_folder, save_name))
+
+
+def call_create_image_features_from_classes_folders():
+
+    class_names_txt = "/home/psrahul/MasterThesis/datasets/CUB_200_2011/CUB_200_2011/classes.txt"
+    images_path = "/home/psrahul/MasterThesis/datasets/CUB_200_2011/CUB_200_2011/train/CUB_200_2011/images/"
+    images_list = "/home/psrahul/MasterThesis/datasets/CUB_200_2011/CUB_200_2011/train/CUB_200_2011/images.txt"
+    save_folder = "/home/psrahul/MasterThesis/repo/Unsupervised-Object-Detection-Plus-CLIP/utils/data_utils/CUB_200_2011/image_features/support_image_features/"
+    save_name = "image_features_support_images_cropped.pt"
+
+    create_image_features_from_classes_folders(
+        images_path, images_list, save_folder, save_name, crop_bounding_box=True)
+
+
+call_create_image_features_from_classes_folders()
