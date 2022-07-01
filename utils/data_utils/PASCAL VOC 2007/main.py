a3d750dd0d3061a28b5bd5ade138f791eb1aa372
PSRahul <rahulpsr95@gmail.com>
2022-07-01 10:16:09 +0200

    add pascalvoc starter code

---
+++ utils/data_utils/PASCAL VOC 2007/main.py
@@ -0,0 +1,17 @@
+import sys
+
+class CreateSupportImagesCropped():
+    def __init__(self) -> :
+        pass
+    
+
+    def create_support_images_cropped(self):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    sys.exit(main())
